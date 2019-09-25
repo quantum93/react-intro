a6c4f4cb082e4ad944708c8599394e59cf68cd77
Student <example@example.com>
2019-09-25 14:21:27 -0700

    More refactoring but little confusing

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,36 +1,21 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
-    // console.log(this.props);
-    // const { name, age, belt } = this.props; //destructing
-    // const { ninjas } = props; // is same as "const ninjas = this.props.ninjas"
-    // const ninjaList = ninjas.map(ninja => {
-    //   if (ninja.age >20) {
-    //     return (
-    //       <div className="ninja" key={ninja.id}>
-    //       <div>Name: {ninja.name}</div>
-    //       <div>Age: {ninja.age}</div>
-    //       <div>Belt: {ninja.belt}</div>
-    //       </div>
-    //     )
-    //   } else {
-    //     return null;
-    //   }
-    // })
-    const ninjaList = ninjas.map(ninja => {
-      return ninja.age >20 ? (
-        <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-        </div>
-      ) : null;
-    })
-    return(
-      <div className="ninja-list">
-        { ninjaList }
-      </div>
-    )
+  return(
+    <div className="ninja-list">
+    {
+      ninjas.map(ninja => {
+        return ninja.age >  20 ? (
+          <div className="ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+          </div>
+        ) : null;
+      })
+    }
+    </div>
+  )
 }
 
 export default Ninjas;
