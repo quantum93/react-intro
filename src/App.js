f36ca0434e490db2d6a4b2eaa53cd5b6fc6b7ea0
Student <example@example.com>
2019-09-25 14:36:17 -0700

    This shows how to make form with class components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -9,11 +10,12 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
-  
+
   render() {
     return (
       <div className="App">
       <Ninjas ninjas={this.state.ninjas}/>
+      <AddNinja />
       </div>
     );
   }
