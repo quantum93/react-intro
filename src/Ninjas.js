d98a6de7f2b1544a52d036e94deb7d644a1d84ee
Student <example@example.com>
2019-09-25 14:08:50 -0700

    conditional output with ternary operators

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,20 +4,28 @@ const Ninjas = ({ninjas}) => {
     // console.log(this.props);
     // const { name, age, belt } = this.props; //destructing
     // const { ninjas } = props; // is same as "const ninjas = this.props.ninjas"
+    // const ninjaList = ninjas.map(ninja => {
+    //   if (ninja.age >20) {
+    //     return (
+    //       <div className="ninja" key={ninja.id}>
+    //       <div>Name: {ninja.name}</div>
+    //       <div>Age: {ninja.age}</div>
+    //       <div>Belt: {ninja.belt}</div>
+    //       </div>
+    //     )
+    //   } else {
+    //     return null;
+    //   }
+    // })
     const ninjaList = ninjas.map(ninja => {
-      if (ninja.age >20) {
-        return (
-          <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-          </div>
-        )
-      } else {
-        return null;
-      }
+      return ninja.age >20 ? (
+        <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+        </div>
+      ) : null;
     })
-
     return(
       <div className="ninja-list">
         { ninjaList }
