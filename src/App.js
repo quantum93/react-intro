c799b198620c8a4285ca032a445dd2a69db9be81
Student <example@example.com>
2019-09-25 13:05:20 -0700

    This is nested Component example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Ninjas />
+      </div>
+    );
+  }
 }
+
 
 export default App;
